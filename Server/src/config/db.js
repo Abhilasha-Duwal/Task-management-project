bcec220f2a7d9766f1d1315a033d6e25abb6f26f
abhilasha <abhilashaduwal@gmail.com>
2025-12-04 20:01:08 +0545

    fix: change port

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -12,12 +12,11 @@ async function connectWithRetry() {
         user: process.env.DB_USER || "root",
         password: process.env.DB_PASSWORD || "root",
         database: process.env.DB_NAME || "mydb",
-        port: 3306,
+        port: process.env.DB_PORT || 3306,
       });
 
       console.log("Connected to MySQL!");
       return db; // <-- return the correct variable
-
     } catch (err) {
       console.log("MySQL not ready, retrying...", retries);
       await new Promise((res) => setTimeout(res, 5000));
