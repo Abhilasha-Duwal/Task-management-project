b10b2cc677bd801c63059e42f879db17766bbe42
abhilasha <abhilashaduwal@gmail.com>
2025-12-04 17:23:39 +0545

    fix: remove get cookie

--- Server/src/middlewares/auth.js
+++ Server/src/middlewares/auth.js
@@ -3,17 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const auth = (req, res, next) => {
-  const token = req.cookies?.token || req.headers?.authorization?.split(" ")[1];
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized. Token missing" });
   }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(403).json({ message: "Invalid Token" });
+    return res.status(403).json({ message: "Invalid or expired token" });
   }
 };
