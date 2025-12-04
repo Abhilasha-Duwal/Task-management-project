f393bb2572f5d7dc9fff92ba3cd7fea2e56c458a
abhilasha <abhilashaduwal@gmail.com>
2025-12-04 14:22:36 +0545

    feat: add routes to render pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import AuthGuard from "./components/AuthGuard";
+import Task from "./pages/Task/Task";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
   return (
-    <div className="App">
-     react app
-    </div>
+   <BrowserRouter>
+      <Routes>
+         {/* Public routes */}
+        <Route path="/login" element={<PublicRoute> <Login /> </PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <AuthGuard>
+              <Task/>
+            </AuthGuard>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
